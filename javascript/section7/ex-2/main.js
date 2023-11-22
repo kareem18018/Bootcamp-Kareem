a90457efd90f435719eeb8aa0a53cebbb64a498d
kareem18018 <kareem18018@gmail.com>
2023-11-22 13:28:55 +0530

    git adding to file

--- javascript/section7/ex-2/main.js
+++ javascript/section7/ex-2/main.js
@@ -3,7 +3,6 @@ const yaml = require('js-yaml');
 
 class Chessboard {
   constructor() {
-    // Initialize an 8x8 chessboard with empty squares
     this.board = Array.from({ length: 8 }, () => Array(8).fill('-'));
   }
 
@@ -37,30 +36,22 @@ class Chessboard {
   }
 }
 
-// Test the Chessboard class
 const chessboard = new Chessboard();
 
-// Print the initial chessboard
 console.log("Initial Chessboard:");
 chessboard.printBoard();
 
-// Move a piece
 chessboard.movePiece(1, 0, 3, 0);
 
-// Kill a piece
 chessboard.killPiece(7, 7);
 
-// Print the updated chessboard
 console.log("\nChessboard after moving and killing:");
 chessboard.printBoard();
 
-// Save the chessboard to a YAML file
 chessboard.saveToFile('chessboard.yaml');
 
-// Load the chessboard from a YAML file
 const loadedChessboard = new Chessboard();
 loadedChessboard.loadFromFile('chessboard.yaml');
 
-// Print the loaded chessboard
 console.log("\nChessboard loaded from file:");
 loadedChessboard.printBoard();
